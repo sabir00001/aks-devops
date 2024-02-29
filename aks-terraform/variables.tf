02bbbb15b553cae9415848f776db26edbb5e6be4
Sabir Ahmed Choudhury <sabirchoudhury3@gmail.com>
2024-02-29 08:46:31 +0530

    added added sshkey

--- aks-terraform/variables.tf
+++ aks-terraform/variables.tf
@@ -14,6 +14,10 @@ variable "resource_group_name_prefix" {
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
+variable "ssh_public_key" {
+  type        = string
+  description = "SSH public key used to access the cluster."
+}
 variable "cluster_name" {
   type    = string
   default = "aks-maf"
