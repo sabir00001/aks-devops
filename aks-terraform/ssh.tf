cce50927e65abeca7bac2fc98c7a5bf5df79f027
Sabir Ahmed Choudhury <sabirchoudhury3@gmail.com>
2024-02-29 10:28:26 +0530

    commented ssh key generation

--- aks-terraform/ssh.tf
+++ aks-terraform/ssh.tf
@@ -1,4 +1,4 @@
-resource "random_pet" "ssh_key_name" {
+/*resource "random_pet" "ssh_key_name" {
   prefix    = "ssh"
   separator = ""
 }
@@ -12,13 +12,13 @@ resource "azapi_resource_action" "ssh_public_key_gen" {
   response_export_values = ["publicKey", "privateKey"]
 }
 
-# resource "azapi_resource" "ssh_public_key" {
-#   type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-#   name      = random_pet.ssh_key_name.id
-#   location  = azurerm_resource_group.aks_rg.location
-#   parent_id = azurerm_resource_group.aks_rg.id
-# }
+resource "azapi_resource" "ssh_public_key" {
+  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  name      = random_pet.ssh_key_name.id
+  location  = azurerm_resource_group.aks_rg.location
+  parent_id = azurerm_resource_group.aks_rg.id
+}
 
 output "key_data" {
   value = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
-}
+}*/
