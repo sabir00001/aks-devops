a69b0e96aab1370bad06cc50912dcace7c7a06fa
Sabir Ahmed Choudhury <sabirchoudhury3@gmail.com>
2024-03-16 14:36:20 +0530

    removed the acr pull

--- aks-terraform/main.tf
+++ aks-terraform/main.tf
@@ -74,10 +74,10 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
 }
 
 # Example of Azure role assignment (if uncommented)
-
+/*
 resource "azurerm_role_assignment" "acr" {
   principal_id                     = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
   scope                            = var.registry_scope
   skip_service_principal_aad_check = true
-}
+ }*/
